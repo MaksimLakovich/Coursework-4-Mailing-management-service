46207716ae01121c12f547399071c17cd9126d7c
Maksim Lakovich <maks.lakovich@gmail.com>
2025-04-12 23:48:20 +0300

    1) В модуле "forms.py" добавлена новая форма - AddNewMessageForm(). 2) Дополнен файл README.md

--- app_mailing/forms.py
+++ app_mailing/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from app_mailing.models import Recipient
+from app_mailing.models import Recipient, Message
 
 
 class AddNewRecipientForm(forms.ModelForm):
@@ -13,6 +13,36 @@ class AddNewRecipientForm(forms.ModelForm):
             "email": forms.EmailInput(attrs={"placeholder": "Введите email (обязательное поле)"}),
             "full_name": forms.TextInput(attrs={"placeholder": "Введите ФИО (опционально)"}),
             "comment": forms.Textarea(attrs={"placeholder": "Введите комментарий (опционально)"}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        """Стилизации полей формы с использованием виджета. Виджеты позволяют настроить внешний вид и поведение полей
+        формы через атрибуты. Это делается с помощью метода attrs.
+            - Добавляем CSS-классы ко всем полям формы.
+            - Убираем параметр 'help_text' со всех полей, чтоб этого больше не было по умолчанию на html-странице."""
+        super().__init__(*args, **kwargs)
+
+        # ШАГ 1: Убираю отображение help_text на странице, так как help_text из model.py дублирует то, что мы
+        # добавили в forms.ModelForm (параметры "placeholder" в widgets).
+        for field_name, field in self.fields.items():
+            field.help_text = None
+            # ШАГ 2: Добавляю класс "form-control" для всех полей, кроме чекбоксов:
+            if not isinstance(field.widget, forms.CheckboxInput):
+                field.widget.attrs["class"] = "form-control text-muted-secondary"
+            # ШАГ 3: Добавляю класс "form-check-input" для чекбоксов:
+            if isinstance(field.widget, forms.CheckboxInput):
+                field.widget.attrs["class"] = "form-check-input"
+
+
+class AddNewMessageForm(forms.ModelForm):
+    """Форма для добавления пользователем нового Сообщения рассылки на странице message_add_update.html"""
+
+    class Meta:
+        model = Message
+        fields = "__all__"
+        widgets = {
+            "message_subject": forms.TextInput(attrs={"placeholder": "Введите тему письма"}),
+            "message_body": forms.Textarea(attrs={"placeholder": "Введите тело письма"}),
         }
 
     def __init__(self, *args, **kwargs):
