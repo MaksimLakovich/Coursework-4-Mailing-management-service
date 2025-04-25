2f25c1bf55aa44bdf2b0ef495eedeedf7d6f2d85
Maksim Lakovich <maks.lakovich@gmail.com>
2025-04-25 12:22:38 +0300

    Дополнен файл README.md

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,8 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     # namespace="app_mailing" это заданное пространство имен в "app_mailing/urls.py" с помощью "CatalogConfig.name"
-    path("mailing/", include("app_mailing.urls", namespace="app_mailing"))
+    path("mailing/", include("app_mailing.urls", namespace="app_mailing")),
+    path("users/", include("users.urls", namespace="users"))
 ]
 
 if settings.DEBUG:
