7d814a2048a2bec6e679d2afcefd111d4733c271
Maksim Lakovich <maks.lakovich@gmail.com>
2025-05-12 15:02:06 +0300

    1) Добавлен функционал восстановления пароля (html, форма, контроллер, url). 2) Дополнен файл README.md

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
+from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LogoutView
-from django.urls import path
+from django.urls import path, reverse_lazy
 
 from users.views import (ActivateAccountView, UserEmailConfirmationSentView,
-                         UserRegisterView, UserStartView, UserLoginView)
+                         UserLoginView, UserRegisterView, UserStartView)
 
 app_name = "users"
 
@@ -13,6 +14,46 @@ urlpatterns = [
     path("email-confirmation-sent/", UserEmailConfirmationSentView.as_view(), name="email_confirmation_sent_page"),
     path("login/", UserLoginView.as_view(), name="login_page"),
     path("logout/", LogoutView.as_view(), name="logout_page"),
-    # path("edit_profile/", CustomEditProfileView.as_view(), name="edit_profile"),
-    # path("change_password/", CustomPasswordChangeView.as_view(), name="change_password"),
+
+    # Часть для сброса пароля с использованием коробочного решения auth_views
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="users/password_reset/password_reset.html",
+            # Django по умолчанию ищет коробочный шаблон в "registration/password_reset_email.html" и ожидает
+            # стандартные имена URL-ов (password_reset_confirm и др.). Так как я изменил name= в urls.py
+            # (сделал "name="password_reset_confirm_page""), то мне нужно использовать собственный шаблон письма
+            # и указать свои url-имя в нём (email_template_name=).
+            email_template_name="users/password_reset/email_confirm_reset.html",
+            # Django по умолчанию в PasswordResetView ожидает, что путь для редиректа после успешной отправки email
+            # будет называться коробочно вот так "password_reset_done", но я его изменил на "password_reset_done_page"
+            # и поэтому необходимо это явно указать, чтоб не было ошибок.
+            success_url=reverse_lazy("users:password_reset_done_page"),
+        ),
+        name="password_reset_page",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="users/password_reset/password_reset_done.html"
+        ),
+        name="password_reset_done_page",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="users/password_reset/password_reset_confirm.html",
+            # Аналогично и для редиректа тут, чтобы после установки нового пароля была переадресация на
+            # "users:password_reset_complete_page", а не на коробочное название, которое ожидает Django по умолчанию.
+            success_url=reverse_lazy("users:password_reset_complete_page"),
+        ),
+        name="password_reset_confirm_page",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="users/password_reset/password_reset_complete.html"
+        ),
+        name="password_reset_complete_page",
+    ),
 ]
